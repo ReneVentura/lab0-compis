78e6bfab8ef87407034774219287b8daf5c63275
ReneVentura <59925574+ReneVentura@users.noreply.github.com>
2023-08-20 11:47:14 -0600

    added http methods

--- backend/main.py
+++ backend/main.py
@@ -2,10 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/tree")
+@app.route("/tree", methods= ["post"])
 def home():
     return "Hello, World!"
-@app.route("/table")
+@app.route("/table", methods= ["post"])
 def home():
     return "Hello, World!"    
 if __name__ == "__main__":
